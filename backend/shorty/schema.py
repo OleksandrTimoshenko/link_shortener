3e2f90eb4f3641f15f663d3580fb7dfede6e5b0e
Oleksandr_Timoshenko <oleksandr.tymoshenko@teamvoy.com>
2021-09-03 12:12:55 +0300

    create mutations

--- backend/shorty/schema.py
+++ backend/shorty/schema.py
@@ -6,4 +6,8 @@ import shortener.schema
 class Query(shortener.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(shortener.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)

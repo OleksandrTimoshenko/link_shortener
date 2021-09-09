5dcade6dc5ab9b28c246a372c2de782a3bb8cb40
Oleksandr_Timoshenko <oleksandr.tymoshenko@teamvoy.com>
2021-09-09 17:16:10 +0300

    add invoke tasks for register and deploy vagrant machine

--- tasks.py
+++ tasks.py
@@ -1,9 +1,22 @@
 from invoke import task
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 @task
 def registry_login(c):
     c.run("docker login -u $GITHUB_USER -p $GITHUB_TOKEN ghcr.io")
+
+# run for the first time only
+@task
+def install_vagrant_plugins(c):
+    c.run("vagrant plugin install vagrant-env")
+
+@task
+def vagrant_provision(c):
+    c.run(
+        "cd deploy; \
+         REGISTRY_TOKEN=$GITHUB_TOKEN \
+	     REGISTRY_USER=$GITHUB_USER \
+	     vagrant up --provision;"
+    )

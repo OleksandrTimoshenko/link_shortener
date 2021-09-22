d1cc0404e8bf50e47d555109e7396edd07fc077b
Oleksandr_Timoshenko <oleksandr.tymoshenko@teamvoy.com>
2021-09-22 09:59:21 +0300

    update main terraform file for using module structure

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,39 +1,17 @@
-variable "aws_access_key" {
-  description = "Access key to your AWS account "
+module "networking" {
+  source    = "./modules/networking"
+  namespace = var.namespace
 }
 
-variable "aws_secret_key" {
-  description = "Secret key to your AWS account "
+module "ssh-key" {
+  source    = "./modules/ssh-key"
+  namespace = var.namespace
 }
 
-variable "aws_region" {
-  default     = "eu-east-1"
-  description = "AWS region"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-09e67e426f25ce0d7" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+module "ec2" {
+  source     = "./modules/ec2"
+  namespace  = var.namespace
+  vpc        = module.networking.vpc
+  sg_pub_id  = module.networking.sg_pub_id
+  key_name   = module.ssh-key.key_name
 }
---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {
+  description = "Access key to your AWS account "
+}
+
+variable "aws_secret_key" {
+  description = "Secret key to your AWS account "
+}
+
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  default     = "TEST"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "eu-east-1"
+  type        = string
+}

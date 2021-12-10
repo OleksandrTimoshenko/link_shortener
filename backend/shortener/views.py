3f171e85d47e786a24ddb19c3ffddbd278bf56b2
Oleksandr_Timoshenko <oleksandr.tymoshenko@teamvoy.com>
2021-12-10 11:15:52 +0200

    add logger into URL view

--- backend/shortener/views.py
+++ backend/shortener/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from .models import URL
+import logging
 
+logger = logging.getLogger(__name__)
 
 def root(request, url_hash):
     url = get_object_or_404(URL, url_hash=url_hash)
     url.clicked()
+    logger.info("URL generated!")
 
     return redirect(url.full_url)
 
